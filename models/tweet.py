d1dda509cd0ad773db37544ac8d31c345e207f14
Gustavo Sanchez <antero105@gmail.com>
2015-05-18 23:50:40 -0300

    Extract information from trafficMcbo twitter Account

---
+++ models/tweet.py
@@ -0,0 +1,12 @@
+from pymongo import MongoClient
+import json
+class TweetModel:
+	"""docstring for Tweet"""
+	def __init__(self):
+		self.client = MongoClient('localhost', 27017)
+		self.db = self.client.trafficMcbo
+		self.collection = self.db.tweets
+		
+	def save(self,tweet):
+		self.collection.save(tweet._json)
+		print 'Saved Success with id : ' + str(tweet.id)
---
+++ trafficMcbo.py
@@ -0,0 +1,49 @@
+import tweepy
+import json
+from models.tweet import TweetModel
+class trafficMcbo:
+    """docstring for trafficMcbo"""
+    def __init__(self):
+        self.consumer_key = 'FOEVRW1yTdOarEftWNt03TMcl'
+        self.consumer_secret = 'O0qYWqG5rvfjbG9rZdSH3BspcYL2deSaccnIbMuVbykcQGoKRI'
+        self.access_token = '79949444-hN4NSBSmefScvvJTqFggi8MnfAwXlXrCSb8cbKi2t'
+        self.access_token_secret = 'dstf86FNm1Lg515gwWRaPG3F3PJ404VhWnCbleFkyi4Nu'
+        self.auth = tweepy.OAuthHandler(self.consumer_key, self.consumer_secret)
+        self.auth.set_access_token(self.access_token, self.access_token_secret)
+        self.api = tweepy.API(self.auth,wait_on_rate_limit=True)
+        self.tweetModel = TweetModel()
+        
+    def initRecolector(self):
+
+        goal = round(5000/200)
+        max_id  = None
+        author = None
+        while goal > 0:
+            tweets = self.api.user_timeline('trafficMcbo',count=200,max_id = max_id,wait_on_rate_limit_notify=True)
+            for tweet in tweets:
+                self.tweetModel.save(tweet)
+            max_id = tweets[-1].id
+            goal = goal - 1
+            print max_id
+        print 'Finish!...'
+            
+        
+        
+    
+    def saveTweet(self,tweet):
+
+        self.tweet.save(tweet)
+
+        
+
+    
+
+
+traffic = trafficMcbo()
+
+traffic.initRecolector()
+        
+
+
+
+
