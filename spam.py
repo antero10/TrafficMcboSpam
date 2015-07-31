1151217d88ada04adc4eb7f1ecf5442a96d0b8e3
Antero10 <antero105@gmail.com>
2015-07-30 21:44:53 -0300

    Only text, lower and remove stop words

--- spam.py
+++ spam.py
@@ -1,36 +1,28 @@
 import pandas as pd
 import re
-from models.tweet import TweetModel
-from sklearn.feature_extraction.text import CountVectorizer
-
+from nltk.corpus import stopwords
 class Spam:
 
     def __init__(self):
-        self.regex = re.compile(ur'via\s+@[a-z]+:\s+')
-        self.tweet = TweetModel()
-        self.data = self.tweet.get()
-        pd.set_option('display.mpl_style', 'default') # Make the graphs a bit prettier
+        self.data_frame = pd.read_csv("tweets.csv", header=0)
+    def clean_data(self):
+
+        self.data_frame['tweet'] = self.data_frame['tweet'].apply(self.only_text)
+        self.data_frame['tweet'] = self.data_frame['tweet'].apply(self.to_lower)
+        self.data_frame['tweet'] = self.data_frame['tweet'].apply(self.remove_stop_words)
+        
+
+    def only_text(self,text):
+        return re.sub("[^a-zA-Z]"," ",text)
+
+    def to_lower(self,text):
+        return text.lower()
 
 
+    def remove_stop_words(self,text):
+        return [w for w in text.split() if not w in stopwords.words('spanish')]
 
-    def cleanData(self):
-        data_frame = pd.DataFrame()
-        data_frame['text'] = map(lambda text: text[1].lower().decode('utf-8'),self.data)
-        data_frame['text'] = data_frame['text'].str.replace("\\",'')
-        data_frame['text'] = data_frame['text'].str.replace(r"^(\s+)?via\s+@\w+:",'')
-        data_frame['text'] = data_frame['text'].str.replace(r"@\w+",'')
-        data_frame['text'] = data_frame['text'].str.strip()
-        data_frame['text'] = data_frame['text'].str.replace(r"(\*|\)|\(|\[|\])",'')
-        data_frame['spam'] = map(lambda spam: spam[2] == 1,self.data)
-        self.logisticRegression(data_frame[:50])
 
-    def logisticRegression(self,data_frame):
-        import statsmodels.api as sm
-        train_cols = data_frame.columns[1:]
-        logit = sm.Logit(data_frame['spam'],data_frame[train_cols])
-        result = logit.fit()
-        print result.summary()
-        print result.conf_int()
 
 spam = Spam()
-spam.cleanData()
+spam.clean_data()
