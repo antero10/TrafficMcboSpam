67c4e8d347c125466a0fa98b400d705f5032843e
Antero10 <antero105@gmail.com>
2015-08-01 18:43:58 -0300

    To lower and text withot via on begining and urls

--- spam.py
+++ spam.py
@@ -5,22 +5,22 @@ class Spam:
 
     def __init__(self):
         self.data_frame = pd.read_csv("tweets.csv", header=0)
+        self.stops = set(stopwords.words("spanish"))
     def clean_data(self):
 
         self.data_frame['tweet'] = self.data_frame['tweet'].apply(self.only_text)
         self.data_frame['tweet'] = self.data_frame['tweet'].apply(self.to_lower)
         self.data_frame['tweet'] = self.data_frame['tweet'].apply(self.remove_stop_words)
-        
+        return self.data_frame
 
     def only_text(self,text):
-        return re.sub("[^a-zA-Z]"," ",text)
+        return re.sub("[^a-zA-Z]|via|(http:\/\/\w+.+?\w+\/?\w+?)"," ",text)
 
     def to_lower(self,text):
         return text.lower()
 
-
     def remove_stop_words(self,text):
-        return [w for w in text.split() if not w in stopwords.words('spanish')]
+        return [w for w in text.split() if not w in self.stops]
 
 
 
